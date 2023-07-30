ed52213ffeca2888cc5d0bd7db45794bbdfa01b9
Luc <luchohieu@gmail.com>
2023-07-30 18:49:15 +0700

    init turbo repo

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+/** @type {import("prettier").Options} */
+const config = {
+  singleQuote: true,
+  jsxSingleQuote: true,
+};
+
+module.exports = config;
---
+++ packages/eslint-config-custom/index.js
@@ -0,0 +1,77 @@
+module.exports = {
+  extends: [
+    'turbo',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    '@next/next/no-html-link-for-pages': 'off',
+    '@typescript-eslint/semi': [
+      'error'
+    ],
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/no-unsafe-member-access': 0,
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_'
+      }
+    ],
+    '@typescript-eslint/no-explicit-any': 1,
+    'no-case-declarations': 0,
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 0,
+    'no-duplicate-imports': [
+      'error',
+      {
+        includeExports: true
+      }
+    ],
+    'indent': [
+      'error',
+      2,
+      {
+        'SwitchCase': 1
+      }
+    ],
+    'quotes': [
+      'error',
+      'single',
+      {
+        'allowTemplateLiterals': true
+      }
+    ],
+    'jsx-quotes': [
+      'error',
+      'prefer-single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'eqeqeq': [
+      'error',
+      'always'
+    ],
+    'no-unused-vars': [
+      'warn',
+      {
+        'argsIgnorePattern': '^_'
+      }
+    ],
+    'object-curly-spacing': [
+      'error',
+      'always'
+    ],
+    'arrow-spacing': [
+      'error',
+      {
+        'before': true,
+        'after': true
+      }
+    ],
+    'no-console': 'warn',
+  },
+};
