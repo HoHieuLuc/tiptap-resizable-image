be592af46d95839dcbc3a92f4d820c37331ffeb1
Luc <luchohieu@gmail.com>
2024-02-27 12:28:55 +0700

    [release] Version: 1.0.0

--- apps/docs/CHANGELOG.md
+++ apps/docs/CHANGELOG.md
@@ -1,5 +1,12 @@
 # docs
 
+## 1.0.8
+
+### Patch Changes
+
+- Updated dependencies
+  - tiptap-extension-resizable-image@1.0.0
+
 ## 1.0.7
 
 ### Patch Changes
--- packages/tiptap-resizable-image/src/index.ts
+++ packages/tiptap-resizable-image/src/index.ts
@@ -3,3 +3,4 @@ export * from './resizable-image.types';
 export { default as ResizableImage } from './resizable-image';
 export { default as ResizableImageComponent } from './ResizableImageComponent';
 export { default as CaptionInput } from './CaptionInput';
+export { default as ImageResizer } from './ImageResizer';
